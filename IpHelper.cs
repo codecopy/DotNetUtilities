acc2790d916ff5f1cec2223cb4aa2f4524d2bbe7
mephistoxq <215152630@qq.com>
2018-06-05 13:49:11 +0800

    IpHelper
    IpHelper

--- IpHelper.cs
+++ IpHelper.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Text.RegularExpressions;
+
 namespace DotNet.Utilities
 {
     /// <summary>
@@ -42,7 +45,63 @@ namespace DotNet.Utilities
         }
         #endregion
 
+        #region 访客IP获取
 
-       
+        public static string GetVisitorsIpAddress()
+        {
+            string result = string.Empty;
+            result = System.Web.HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            //如果使用代理，获取真实IP
+            if (result != null && result.IndexOf(".") == -1)
+            {
+                result = null;
+            }else if (result != null)
+            {
+                if (result.IndexOf(",") != -1)
+                {
+                    //有“,”，可能多个代理，取第一个不是内网的IP
+                    result = result.Replace(" ", "").Replace("'", "");
+                    string[] temparyip = result.Split(",;".ToCharArray());
+                    for (int i = 0; i < temparyip.Length; i++)
+                    {
+                        if (IsIpAddress(temparyip[i]) && temparyip[i].Substring(0, 3) != "10." &&
+                            temparyip[i].Substring(0, 7) != "192.168" && temparyip[i].Substring(0, 7) != "172.16.")
+                        {
+                            return temparyip[i];//找到不是内网的地址
+                        }
+                    }
+                }else if (IsIpAddress(result)) //代理即是IP格式
+                    return result;
+                else
+                {
+                    result = null;//代理中的内容 非IP，取IP
+                }
+            }
+            if (null == result || result == string.Empty)
+                result = System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+
+            if (result == null || result == string.Empty)
+            {
+                result = System.Web.HttpContext.Current.Request.UserHostAddress;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 判断是否是IP地址格式 0.0.0.0
+        /// </summary>
+        /// <param name="ipstr"></param>
+        /// <returns></returns>
+        private static bool IsIpAddress(string ipstr)
+        {
+            if(string.IsNullOrEmpty(ipstr)||ipstr.Length<7||ipstr.Length>15)
+                return false;
+            string regformat = @"^\d{1,3}[\.]\d{1,3}[\.]\d{1,3}[\.]\d{1,3}$";
+            Regex regex = new Regex(regformat,RegexOptions.IgnoreCase);
+            return regex.IsMatch(ipstr);
+        }
+        #endregion
+
     }
 }
